8ad880dcd4ea3a8d25f87eef053a49277015e7eb
lxp <1185307021@qq.com>
2023-08-28 11:45:11 +0800

    vue项目搭建

---
+++ vue/vue项目/新手实践/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip=false
+
+new Vue({
+    el:"#app",
+    // template:'<App></App>',
+    // components:{App},
+    render:h=>h(App),
+})
