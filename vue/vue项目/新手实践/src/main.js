ca3f248c75cc91b620f0eeb76b399c02136083e1
lxp <1185307021@qq.com>
2023-08-29 11:15:42 +0800

    vue项目加注释

--- vue/vue项目/新手实践/src/main.js
+++ vue/vue项目/新手实践/src/main.js
@@ -1,3 +1,4 @@
+//import Vue from 'vue/dist/vue'//完整的vue
 import Vue from 'vue'
 import App from './App.vue'
 
@@ -5,7 +6,13 @@ Vue.config.productionTip=false
 
 new Vue({
     el:"#app",
+    //写法一：需要调用完整的vue
     // template:'<App></App>',
     // components:{App},
+    //写法二:
     render:h=>h(App),
+    // render(createElement)
+    // {
+    //     return createElement('h1','你好啊')
+    // }
 })
